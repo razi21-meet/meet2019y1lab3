20d4ef50440919a2069cdcbb7a544a2b137de1c8
razi21-meet <razi21@meet.mit.edu>
2019-07-23 05:32:45 -0400

    Stopping here

---
+++ lab_3.py
@@ -0,0 +1,14 @@
+print('hello world')
+print("hello wolrd")
+print("hello tamar")
+print("hello gil")
+#print('hello world')
+user = input("say something! ")
+string = 'go to surf'.upper()
+user = 'DABK'.lower()
+happy = 'hAVE fUN'.swapcase()
+print(user)
+print(string)
+print(happy)
+
+
